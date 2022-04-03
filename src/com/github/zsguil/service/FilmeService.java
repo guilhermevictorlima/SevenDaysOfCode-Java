60efb0480835b37e10bfaa0d883da3b8e4e41b5f
zsGuil <guilhermevictorrocha@gmail.com>
2022-04-02 22:46:04 -0300

    Adicionando consulta a séries e criando método padrão de listagem

--- src/com/github/zsguil/service/FilmeService.java
+++ src/com/github/zsguil/service/FilmeService.java
@@ -13,10 +13,22 @@ public class FilmeService {
     private final HttpService httpService = new HttpService();
 
     public List<Filme> getTopFilmesImdb() throws IOException, InterruptedException, ParseException {
-        JSONArray jsonFilmes = (JSONArray) httpService.parseJson(httpService.get("https://imdb-api.com/pt-BR/API/Top250Movies/"))
-            .get("items");
+        return listar("https://imdb-api.com/pt-BR/API/Top250Movies/", "items");
+    }
 
-        return jsonFilmes
+    public List<Filme> getTopSeriesImdb() throws IOException, InterruptedException, ParseException {
+        return listar("https://imdb-api.com/pt-BR/API/Top250TVs/", "items");
+    }
+
+    private List<Filme> listar(String url, String arrayResultado) throws IOException, InterruptedException, ParseException {
+        JSONArray json = (JSONArray) httpService.parseJson(httpService.get(url))
+            .get(arrayResultado);
+
+        return parseFilmes(json);
+    }
+
+    private List<Filme> parseFilmes(JSONArray json) {
+        return json
             .stream()
             .map(item -> Filme.from((JSONObject) item))
             .toList();
